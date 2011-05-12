c0911d2e63ab083910766ed1dce3ccbb1cfed683
Jim Kingdon <jkingdon@localhost.localdomain>
2011-05-12 07:56:59 -0400

    Spec immutability.

--- lib/hilbert.rb
+++ lib/hilbert.rb
@@ -4,10 +4,25 @@ require 'ffi'
 module Hilbert
   extend FFI::Library
   ffi_lib '../hilbert-kernel/src/.libs/libhilbert.so'
-  ModuleType = enum :module_type, [:interface, :proof]
+  ModuleType = enum [:interface, :proof]
+
+  # Not sure why an enum didn't work. Because the values are negative?
+  SUCCESS = 0
+  NOMEM = -1
+  IMMUTABLE = -2
+  INVALID_MODULE = -3
+  INVALID_HANDLE = -4
+  COUNT_MISMATCH = -5
+  INVALID_MAPPING = -6
+  MAPPING_CLASH = -7
+  NO_EQUIVALENCE = -8
+  INTERNAL = -99
+
   attach_function :hilbert_module_create, [ ModuleType ], :pointer
   attach_function :hilbert_module_free, [ :pointer ], :void
   attach_function :hilbert_module_gettype, [ :pointer ], ModuleType
-  attach_function :hilbert_module_makeimmutable, [ :pointer ], :int
+  attach_function :hilbert_module_makeimmutable, [ :pointer ], :int 
+  attach_function :hilbert_module_isimmutable,
+    [ :pointer, :pointer ], :int
 end
 
--- spec/hilbert_spec.rb
+++ spec/hilbert_spec.rb
@@ -2,11 +2,64 @@ require File.expand_path("#{File.dirname(__FILE__)}/spec_helper")
 
 describe Hilbert do
   it "can create and retrieve type" do
-    begin
-      proof = Hilbert.hilbert_module_create(:proof)
-      Hilbert.hilbert_module_gettype(proof).should == :proof
-    ensure
-      Hilbert.hilbert_module_free(proof) if proof
+    proof = Hilbert.hilbert_module_create(:proof)
+    Hilbert.hilbert_module_gettype(proof).should == :proof
+  end
+
+  it "can free a module" do
+    # For most of these tests, we are content to leak memory.
+    # Here we just check that free doesn't blow up.
+    proof = Hilbert.hilbert_module_create(:proof)
+    Hilbert.hilbert_module_free(proof)
+  end
+
+  it "can mark an interface as immutable" do
+    interface = Hilbert.hilbert_module_create(:interface)
+    Hilbert.hilbert_module_makeimmutable(interface).should ==
+      Hilbert::SUCCESS
+  end
+
+  it "cannot mark a proof module as immutable" do
+    proof = Hilbert.hilbert_module_create(:proof)
+    Hilbert.hilbert_module_makeimmutable(proof).should ==
+      Hilbert::INVALID_MODULE
+  end
+
+  it "get an error if the module is already immutable" do
+    interface = Hilbert.hilbert_module_create(:interface)
+    Hilbert.hilbert_module_makeimmutable(interface)
+    Hilbert.hilbert_module_makeimmutable(interface).should ==
+      Hilbert::IMMUTABLE
+  end
+
+  it "can tell me a mutable interface is not immutable" do
+    interface = Hilbert.hilbert_module_create(:interface)
+
+    FFI::MemoryPointer.new(:int, 1) do |errcodep|
+      Hilbert.hilbert_module_isimmutable(interface, errcodep).
+        should == 0
+      errcodep.get_int(0).should == Hilbert::SUCCESS
+    end
+  end
+
+  it "can tell me an immutable module is immutable" do
+    interface = Hilbert.hilbert_module_create(:interface)
+    Hilbert.hilbert_module_makeimmutable(interface)
+
+    FFI::MemoryPointer.new(:int, 1) do |errcodep|
+      Hilbert.hilbert_module_isimmutable(interface, errcodep).
+        should == 1
+      errcodep.get_int(0).should == Hilbert::SUCCESS
+    end
+  end
+
+  it "says that proof modules are not immutable" do
+    proof = Hilbert.hilbert_module_create(:proof)
+
+    FFI::MemoryPointer.new(:int, 1) do |errcodep|
+      Hilbert.hilbert_module_isimmutable(proof, errcodep).
+        should == 0
+      errcodep.get_int(0).should == Hilbert::SUCCESS
     end
   end
 end
