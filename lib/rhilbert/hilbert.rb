8ce7712d8046104019ece313d0138829482080ac
Jim Kingdon <jkingdon@localhost.localdomain>
2011-05-16 10:12:57 -0400

    Change Hilbert to RHilbert::Hilbert.

--- lib/rhilbert/hilbert.rb
+++ lib/rhilbert/hilbert.rb
@@ -1,43 +1,45 @@
 require 'rubygems'
 require 'ffi'
 
-module Hilbert
-  extend FFI::Library
-  ffi_lib '../hilbert-kernel/src/.libs/libhilbert.so'
-  ModuleType = enum [:interface, :proof]
+module RHilbert
+  module Hilbert
+    extend FFI::Library
+    ffi_lib '../hilbert-kernel/src/.libs/libhilbert.so'
+    ModuleType = enum [:interface, :proof]
 
-  # Not sure why an enum didn't work. Because the values are negative?
-  SUCCESS = 0
-  NOMEM = -1
-  IMMUTABLE = -2
-  INVALID_MODULE = -3
-  INVALID_HANDLE = -4
-  COUNT_MISMATCH = -5
-  INVALID_MAPPING = -6
-  MAPPING_CLASH = -7
-  NO_EQUIVALENCE = -8
-  INTERNAL = -99
+    # Not sure why an enum didn't work. Because the values are negative?
+    SUCCESS = 0
+    NOMEM = -1
+    IMMUTABLE = -2
+    INVALID_MODULE = -3
+    INVALID_HANDLE = -4
+    COUNT_MISMATCH = -5
+    INVALID_MAPPING = -6
+    MAPPING_CLASH = -7
+    NO_EQUIVALENCE = -8
+    INTERNAL = -99
 
-  attach_function :hilbert_module_create, [ ModuleType ], :pointer
-  attach_function :hilbert_module_free, [ :pointer ], :void
-  attach_function :hilbert_module_gettype, [ :pointer ], ModuleType
-  attach_function :hilbert_module_makeimmutable, [ :pointer ], :int 
-  attach_function :hilbert_module_isimmutable,
-    [ :pointer, :pointer ], :int
-  attach_function :hilbert_module_getobjects,
-    [ :pointer, :pointer, :pointer ], :pointer
-  attach_function :hilbert_array_free, [ :pointer ], :void
+    attach_function :hilbert_module_create, [ ModuleType ], :pointer
+    attach_function :hilbert_module_free, [ :pointer ], :void
+    attach_function :hilbert_module_gettype, [ :pointer ], ModuleType
+    attach_function :hilbert_module_makeimmutable, [ :pointer ], :int 
+    attach_function :hilbert_module_isimmutable,
+      [ :pointer, :pointer ], :int
+    attach_function :hilbert_module_getobjects,
+      [ :pointer, :pointer, :pointer ], :pointer
+    attach_function :hilbert_array_free, [ :pointer ], :void
 
-  # This is an addition to FFI, but rather than monkey-patching,
-  # I'm putting it here for now. Should be forking FFI I suppose.
-  def self.get_size_t(memory_pointer, array_index)
-    size = memory_pointer.type_size
-    if size == 4
-      return memory_pointer.get_uint32(array_index)
-    elsif size == 8
-      return memory_pointer.get_uint64(array_index)
-    else
-      raise "Don't know how to get a size_t of #{size} bytes"
+    # This is an addition to FFI, but rather than monkey-patching,
+    # I'm putting it here for now. Should be forking FFI I suppose.
+    def self.get_size_t(memory_pointer, array_index)
+      size = memory_pointer.type_size
+      if size == 4
+        return memory_pointer.get_uint32(array_index)
+      elsif size == 8
+        return memory_pointer.get_uint64(array_index)
+      else
+        raise "Don't know how to get a size_t of #{size} bytes"
+      end
     end
   end
 end
--- spec/rhilbert/hilbert_spec.rb
+++ spec/rhilbert/hilbert_spec.rb
@@ -1,76 +1,76 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 
-describe Hilbert do
+describe RHilbert::Hilbert do
   it "can create and retrieve type" do
-    proof = Hilbert.hilbert_module_create(:proof)
-    Hilbert.hilbert_module_gettype(proof).should == :proof
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
+    RHilbert::Hilbert.hilbert_module_gettype(proof).should == :proof
   end
 
   it "can free a module" do
     # For most of these tests, we are content to leak memory.
     # Here we just check that free doesn't blow up.
-    proof = Hilbert.hilbert_module_create(:proof)
-    Hilbert.hilbert_module_free(proof)
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
+    RHilbert::Hilbert.hilbert_module_free(proof)
   end
 
   it "can mark an interface as immutable" do
-    interface = Hilbert.hilbert_module_create(:interface)
-    Hilbert.hilbert_module_makeimmutable(interface).should ==
-      Hilbert::SUCCESS
+    interface = RHilbert::Hilbert.hilbert_module_create(:interface)
+    RHilbert::Hilbert.hilbert_module_makeimmutable(interface).should ==
+      RHilbert::Hilbert::SUCCESS
   end
 
   it "cannot mark a proof module as immutable" do
-    proof = Hilbert.hilbert_module_create(:proof)
-    Hilbert.hilbert_module_makeimmutable(proof).should ==
-      Hilbert::INVALID_MODULE
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
+    RHilbert::Hilbert.hilbert_module_makeimmutable(proof).should ==
+      RHilbert::Hilbert::INVALID_MODULE
   end
 
   it "get an error if the module is already immutable" do
-    interface = Hilbert.hilbert_module_create(:interface)
-    Hilbert.hilbert_module_makeimmutable(interface)
-    Hilbert.hilbert_module_makeimmutable(interface).should ==
-      Hilbert::IMMUTABLE
+    interface = RHilbert::Hilbert.hilbert_module_create(:interface)
+    RHilbert::Hilbert.hilbert_module_makeimmutable(interface)
+    RHilbert::Hilbert.hilbert_module_makeimmutable(interface).should ==
+      RHilbert::Hilbert::IMMUTABLE
   end
 
   it "can tell me a mutable interface is not immutable" do
-    interface = Hilbert.hilbert_module_create(:interface)
+    interface = RHilbert::Hilbert.hilbert_module_create(:interface)
 
     FFI::MemoryPointer.new(:int, 1) do |errcodep|
-      Hilbert.hilbert_module_isimmutable(interface, errcodep).
+      RHilbert::Hilbert.hilbert_module_isimmutable(interface, errcodep).
         should == 0
-      errcodep.get_int(0).should == Hilbert::SUCCESS
+      errcodep.get_int(0).should == RHilbert::Hilbert::SUCCESS
     end
   end
 
   it "can tell me an immutable module is immutable" do
-    interface = Hilbert.hilbert_module_create(:interface)
-    Hilbert.hilbert_module_makeimmutable(interface)
+    interface = RHilbert::Hilbert.hilbert_module_create(:interface)
+    RHilbert::Hilbert.hilbert_module_makeimmutable(interface)
 
     FFI::MemoryPointer.new(:int, 1) do |errcodep|
-      Hilbert.hilbert_module_isimmutable(interface, errcodep).
+      RHilbert::Hilbert.hilbert_module_isimmutable(interface, errcodep).
         should == 1
-      errcodep.get_int(0).should == Hilbert::SUCCESS
+      errcodep.get_int(0).should == RHilbert::Hilbert::SUCCESS
     end
   end
 
   it "says that proof modules are not immutable" do
-    proof = Hilbert.hilbert_module_create(:proof)
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
 
     FFI::MemoryPointer.new(:int, 1) do |errcodep|
-      Hilbert.hilbert_module_isimmutable(proof, errcodep).
+      RHilbert::Hilbert.hilbert_module_isimmutable(proof, errcodep).
         should == 0
-      errcodep.get_int(0).should == Hilbert::SUCCESS
+      errcodep.get_int(0).should == RHilbert::Hilbert::SUCCESS
     end
   end
 
   it "can set ancillary data" do
     pending("this API is annoying from FFI; lots of memory allocation")
-    proof = Hilbert.hilbert_module_create(:proof)
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
 
     FFI::MemoryPointer.new(:int, 1) do |ancillary|
-      Hilbert.hilbert_module_setancillary(proof, ancillary, 0).
+      RHilbert::Hilbert.hilbert_module_setancillary(proof, ancillary, 0).
         should == 0
-      Hilbert.hilbert_module_getancillary(proof).should == ancillary
+      RHilbert::Hilbert.hilbert_module_getancillary(proof).should == ancillary
     end
   end
 
@@ -83,15 +83,15 @@ describe Hilbert do
   it "hilbert_module_export"
 
   it "with an empty proof module has no objects" do
-    proof = Hilbert.hilbert_module_create(:proof)
+    proof = RHilbert::Hilbert.hilbert_module_create(:proof)
     FFI::MemoryPointer.new(:int, 1) do |errcodep|
       FFI::MemoryPointer.new(:size_t, 1) do |countp|
-        objects = Hilbert.hilbert_module_getobjects(
+        objects = RHilbert::Hilbert.hilbert_module_getobjects(
           proof, countp, errcodep
         )
-        errcodep.get_int(0).should == Hilbert::SUCCESS
-        Hilbert.get_size_t(countp, 0).should == 0
-        Hilbert.hilbert_array_free(objects)
+        errcodep.get_int(0).should == RHilbert::Hilbert::SUCCESS
+        RHilbert::Hilbert.get_size_t(countp, 0).should == 0
+        RHilbert::Hilbert.hilbert_array_free(objects)
       end
     end
   end
