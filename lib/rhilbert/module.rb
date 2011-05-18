7c9155e4bafd3c15fd8fb9abb52e239f580f0d09
Jim Kingdon <jkingdon@localhost.localdomain>
2011-05-17 21:00:48 -0400

    Can create interface or proof modules and ask what kind they are.

--- lib/rhilbert/module.rb
+++ lib/rhilbert/module.rb
@@ -1,16 +1,24 @@
 module RHilbert
   class Module
-    def self.create
-      mmodule = new
-      yield(mmodule)
+    def self.create(module_type, &block)
+      mmodule = new(module_type)
+      block.call(mmodule)
       mmodule.free
+    end
+
+    def self.interface(&block)
+      create(:interface, &block)
+    end
+
+    def self.proof(&block)
+      create(:proof, &block)
     end
 
     private_class_method :new
 
     private
-    def initialize
-      @hilbert_module = Hilbert.hilbert_module_create(:proof)
+    def initialize(module_type)
+      @hilbert_module = Hilbert.hilbert_module_create(module_type)
     end
 
     def hilbert_module
@@ -30,6 +38,16 @@ module RHilbert
     def free
       RHilbert::Hilbert.hilbert_module_free(@hilbert_module)
       @hilbert_module = nil
+    end
+
+    def interface?
+      RHilbert::Hilbert.
+        hilbert_module_gettype(@hilbert_module) == :interface
+    end
+
+    def proof?
+      RHilbert::Hilbert.
+        hilbert_module_gettype(@hilbert_module) == :proof
     end
   end
 end
--- spec/rhilbert/module_spec.rb
+++ spec/rhilbert/module_spec.rb
@@ -2,34 +2,41 @@ require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 
 describe RHilbert::Module do
   it "can allocate a module with a block" do
-    RHilbert::Module.create do |m|
+    RHilbert::Module.proof do |m|
       m.should_not be_nil
     end
   end
 
   it "frees the module when the block exits" do
     RHilbert::Hilbert.should_receive(:hilbert_module_free)
-    RHilbert::Module.create do |m|
+    RHilbert::Module.proof do |m|
     end
   end
 
   it "blows up if we let the reference escape from the block" do
     escaper = nil
-    RHilbert::Module.create do |m|
+    RHilbert::Module.proof do |m|
       escaper = m
     end
     lambda { escaper.immutable? }.should raise_error(
       "Attempt to access rhilbert module outside its block")
   end
 
-  it "does not make it easy to leak/corrupt memory by calling new" do
+  it "does not make it easy to leak memory by calling new" do
     lambda do
       RHilbert::Module.new
     end.should raise_error
   end
 
+  it "interface module is an interface and not a proof module" do
+    RHilbert::Module.interface do |m|
+      m.should be_interface
+      m.should_not be_proof
+    end
+  end
+
   it "is not immutable by default" do
-    RHilbert::Module.create do |m|
+    RHilbert::Module.proof do |m|
       m.should_not be_immutable
     end
   end
