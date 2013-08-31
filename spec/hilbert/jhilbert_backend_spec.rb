967cdcae48809ae4dbf41a432b382f7dabb90149
Jim Kingdon <jkingdon@localhost>
2013-08-31 14:09:16 -0400

    Some old changes to jhilbert integration
    Not sure how complete.

--- spec/hilbert/jhilbert_backend_spec.rb
+++ spec/hilbert/jhilbert_backend_spec.rb
@@ -12,5 +12,12 @@ describe "able to talk to JHilbert" do
       Hilbert::JHilbert.process_file("./Main/N/o/S/NoSuchFile")
     end.should raise_error(/FileNotFound/)
   end
+
+  it "can import an interface" do
+#    lambda do
+      proof = Hilbert::JHilbert.new_proof_module
+      proof.import("./Interface/S/a/m/Sample")
+#    end.should_not raise_error
+  end
 end
 
