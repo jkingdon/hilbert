bb6e0a8f39db722404e6c77463803ebad7f4717e
Jim Kingdon <jkingdon@localhost>
2013-09-02 13:07:32 -0700

    Track column and line numbers.
    Also add a bunch of pending specs for cases we don't yet handle.

--- lib/hilbert/verifier/tokenizer.rb
+++ lib/hilbert/verifier/tokenizer.rb
@@ -1,15 +1,86 @@
+class Token
+  attr_reader :line, :start_column, :end_column, :text
+
+  def initialize(line, start_column)
+    @line = line
+    @start_column = start_column
+    @end_column = start_column
+    @text = ""
+  end
+
+  def add_character(character)
+    @end_column += 1
+    @text += character
+  end
+end
+
+class EndOfFile
+  attr_reader :line
+
+  def initialize(line, column)
+    @line = line
+    @column = column
+  end
+
+  def start_column
+    @column
+  end
+
+  def end_column
+    @column
+  end
+
+  def text
+    nil
+  end
+end
+
 class Tokenizer
   def initialize(stream)
     @stream = stream
+    @line = 1
+    @column = 1
+    @token = nil
+  end
+
+  def read
+    while true
+      if @stream.eof?
+        # TODO: why this and the nil case both?
+        @token ||= EndOfFile.new(@line, @column)
+        return
+      end
+
+      character = @stream.readchar
+      case character
+        when ' ', "\t", "\n"
+          # do nothing
+        when nil
+          @token ||= EndOfFile.new(@line, @column)
+        else
+          @token ||= Token.new(@line, @column)
+          @token.add_character(character)
+      end
+      @column += 1
+    end
   end
 
   def next_token
-    result = @stream.read
-    if @stream.eof? && result == ""
-      nil
-    else
-      result.strip
-    end
+    @token = nil
+    read
+    @token.text
+  end
+
+  def current_line
+    1
+  end
+
+  def current_column_start
+    @token.start_column
+  end
+
+  def current_column_end
+    @token.end_column
   end
 
 end
--- spec/hilbert/verifier/tokenizer_spec.rb
+++ spec/hilbert/verifier/tokenizer_spec.rb
@@ -18,5 +18,57 @@ describe "Tokenizer" do
       expect(tokenizer.next_token).to eq(nil)
     end
   end
+
+  context "with two tokens" do
+    let(:input) { "foo bar" }
+    it "returns them" do
+      pending("let's work on lines and come back to this")
+      expect(tokenizer.next_token).to eq("foo")
+      expect(tokenizer.next_token).to eq("bar")
+      expect(tokenizer.next_token).to eq(nil)
+    end
+
+    it "gives line/column numbers for each"
+  end
+
+  context "with a parenthesized expression"
+
+  context "with some leading and trailing whitespace" do
+    let(:input) { "  pi  " }
+    it "can give line and column numbers" do
+      expect(tokenizer.next_token).to eq("pi")
+      expect(tokenizer.current_line).to eq(1)
+      expect(tokenizer.current_column_start).to eq(3)
+      expect(tokenizer.current_column_end).to eq(5)
+    end
+
+    it "gives the correct position of the end of the file" do
+      tokenizer.next_token
+      expect(tokenizer.next_token).to eq(nil)
+      expect(tokenizer.current_line).to eq(1)
+      expect(tokenizer.current_column_start).to eq(7)
+      expect(tokenizer.current_column_end).to eq(7)
+    end
+  end
+
+  context "with several lines" do
+    it "can give line and column numbers"
+  end
+
+  context "with multibyte characters" do
+    it "returns column numbers in characters, not bytes"
+  end
+
+  context "with mismatched parentheses" do
+    it "gives an error"
+    it "includes the line/column number of the unclosed parenthesis"
+  end
+
+  context "with comments" do
+    it "ignores to end of line"
+    it "does not ignore subsequent lines"
+    it "does not ignore the line prior to the comment start"
+    it "does not try to make parentheses be balanced within a comment"
+  end
 end
 
